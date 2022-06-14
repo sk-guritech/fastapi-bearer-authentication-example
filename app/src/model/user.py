74ccc55f9f55666e6308268578f64f84c4cab6b4
@GuriTech <guri95saka22@gmail.com>
2022-06-14 16:30:23 -0400

    [fix]fix type

--- app/src/model/user.py
+++ app/src/model/user.py
@@ -1,12 +1,13 @@
+from sqlalchemy import String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 from sqlalchemy.schema import Column
-from sqlalchemy.types import VARCHAR
 
 class User(Base):
     __tablename__ = 'users'
-    ulid = Column(VARCHAR(26), primary_key=True)
-    password_hash = Column(VARCHAR(60), nullable=False)
-    name = Column(VARCHAR(255), nullable=False)
-    email_address = Column(VARCHAR(255), nullable=False)
+    ulid = Column(String(26), primary_key=True)
+    password_hash = Column(String(60), nullable=False)
+    name = Column(String(255), nullable=False)
+    email_address = Column(String(255), nullable=False)
+    
