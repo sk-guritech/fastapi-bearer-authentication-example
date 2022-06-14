e025658eddcd1db64ee8b384c748354d9534f70c
@GuriTech <guri95saka22@gmail.com>
2022-06-14 16:31:19 -0400

    [update]added /logout & /refresh api

--- app/src/router/authenticate_api.py
+++ app/src/router/authenticate_api.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
+from typing import Dict
 import uuid
 from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import redis
 from sqlalchemy.exc import NoResultFound
 from passlib.hash import bcrypt
+import ulid
 from src import db_session, redis_session
 from src.model.user import User
 from jose import JWTError, jwt
@@ -14,6 +17,7 @@ ACCESS_TOKEN_EXPIRE_MINUTE = 60
 REFRESH_TOKEN_EXPIRE_MINUTE = 1440
 
 api_router = APIRouter()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="authenticate")
 
 def unauthorized_callback():
     raise HTTPException(
@@ -33,7 +37,7 @@ def fetch_user_record(username: str, password: str) -> User:
     except (NoResultFound, ValueError):
         unauthorized_callback()
     
-def generate_access_token(ulid: str) -> str:
+def generate_access_token(ulid: str) -> tuple[str, str]:
     claims = {
         "sub": ulid,
         "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTE),
@@ -42,28 +46,62 @@ def generate_access_token(ulid: str) -> str:
     }
     
     encoded_jwt = jwt.encode(claims, SECRET_KEY, algorithm=JWT_ENCODE_ALGORITHM)
-    redis_session.set(ulid + ":access_token",claims["jti"])
     
-    return encoded_jwt
+    return encoded_jwt, claims["jti"]
 
-def generate_refresh_token(ulid: str) -> str:
+def generate_refresh_token(ulid: str) -> tuple[str, str]:
     claims = {
         "sub": ulid,
         "exp": datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTE),
         "jti": ulid + ":" + uuid.uuid4().hex,
-        "grant": "access"
+        "grant": "refresh"
     }
     
     encoded_jwt = jwt.encode(claims, SECRET_KEY, algorithm=JWT_ENCODE_ALGORITHM)
-    redis_session.set(ulid + ":refresh_token",claims["jti"])
     
-    return encoded_jwt
+    return encoded_jwt, claims["jti"]
+
+def register_authenticate_token_jtis_to_redis(ulid: str, access_token_jti: str, refresh_token_jti: str) -> None:
+    redis_session.set(ulid + ":access_token", access_token_jti)
+    redis_session.set(ulid + ":refresh_token",refresh_token_jti)
+    
 
 @api_router.post("/authenticate")
-def fetch_authenticate_tokens(form_data: OAuth2PasswordRequestForm = Depends()):
+async def fetch_authenticate_tokens(form_data: OAuth2PasswordRequestForm = Depends()):
     user = fetch_user_record(form_data.username, form_data.password)
-    access_token = generate_access_token(user.ulid)
-    refresh_token = generate_refresh_token(user.ulid)
+    access_token, access_token_jti = generate_access_token(str(user.ulid))
+    refresh_token, refresh_token_jti = generate_refresh_token(str(user.ulid))
+    register_authenticate_token_jtis_to_redis(str(user.ulid), access_token_jti, refresh_token_jti)
+        
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
+
+@api_router.post("/refresh")
+async def refresh_authenticate_tokens(refresh_token: str = Depends(oauth2_scheme)):
+    claims = jwt.decode(refresh_token, SECRET_KEY, algorithms=JWT_ENCODE_ALGORITHM)
+    
+    if claims["grant"] != "refresh":
+        unauthorized_callback()
+    
+    ulid = claims["sub"]
+    
+    if redis_session.get(ulid + ":refresh_token") is None:
+        unauthorized_callback()
+    
+    access_token, access_token_jti = generate_access_token(ulid)
+    refresh_token, refresh_token_jti = generate_refresh_token(ulid)
+    register_authenticate_token_jtis_to_redis(ulid, access_token_jti, refresh_token_jti)
     
     return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
+
+@api_router.post("/logout")
+async def delete_authenticate_token_jtis_from_redis(access_token: str = Depends(oauth2_scheme)):
+    claims = jwt.decode(access_token, SECRET_KEY, algorithms=JWT_ENCODE_ALGORITHM)
     
+    if claims["grant"] != "access":
+        unauthorized_callback()
+        
+    ulid = claims["sub"]
+    redis_session.delete(ulid + ":access_token")
+    redis_session.delete(ulid + ":refresh_token")
+    
+    return {}
