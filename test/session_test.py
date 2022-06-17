008d68f63da1fffab6644b060b0913d00bb6b38d
@GuriTech <guri95saka22@gmail.com>
2022-06-17 00:23:04 -0400

    [update]simplified

--- test/session_test.py
+++ test/session_test.py
@@ -6,82 +6,66 @@ from urllib3.exceptions import InsecureRequestWarning
 
 urllib3.disable_warnings(InsecureRequestWarning)
 
-print('=' * 20 + '  Authenticate  ' + '=' * 20)
-json_response = requests.post(
-    'https://127.0.0.1/authenticate', data={
-        'username': 'johndoe', 'password': 'secret',
-    }, verify=False,
-).json()
-access_token = json_response['access_token']
-refresh_token = json_response['refresh_token']
-print(f'access_token  : {access_token}')
-print(f'refresh_token : {refresh_token}')
 
-print('=' * 20 + '  Refresh Token  ' + '=' * 19)
-json_response = requests.post(
-    'https://127.0.0.1/refresh', headers={
-        'Authorization': f'Bearer {refresh_token}',
-    }, verify=False,
-).json()
-access_token = json_response['access_token']
-refresh_token = json_response['refresh_token']
-print(f'access_token  : {access_token}')
-print(f'refresh_token : {refresh_token}')
+def call_authentication(username: str, password: str) -> tuple[str, str]:
+    print('=' * 20 + '  Authenticate  ' + '=' * 20)
+    json_response = requests.post(
+        'https://127.0.0.1/authenticate', data={
+            'username': username, 'password': password,
+        }, verify=False,
+    ).json()
+    access_token = json_response['access_token']
+    refresh_token = json_response['refresh_token']
 
-print('=' * 20 + '  Refresh Token  ' + '=' * 19)
-json_response = requests.post(
-    'https://127.0.0.1/refresh', headers={
-        'Authorization': f'Bearer {refresh_token}',
-    }, verify=False,
-).json()
-access_token = json_response['access_token']
-refresh_token = json_response['refresh_token']
-print(f'access_token  : {access_token}')
-print(f'refresh_token : {refresh_token}')
+    print(f'access_token  : {access_token}')
+    print(f'refresh_token : {refresh_token}')
 
-print('=' * 20 + '  Logout  ' + '=' * 26)
-json_response = requests.post(
-    'https://127.0.0.1/logout', headers={
-        'Authorization': f'Bearer {access_token}',
-    }, verify=False,
-).json()
+    return access_token, refresh_token
 
-print('=' * 20 + '  Refresh Token  ' + '=' * 19)
-response = requests.post(
-    'https://127.0.0.1/refresh',
-    headers={'Authorization': f'Bearer {refresh_token}'}, verify=False,
-)
-print(response)
 
-print('=' * 20 + '  Authenticate  ' + '=' * 20)
-json_response = requests.post(
-    'https://127.0.0.1/authenticate', data={
-        'username': 'johndoe', 'password': 'secret',
-    }, verify=False,
-).json()
-access_token = json_response['access_token']
-refresh_token = json_response['refresh_token']
-print(f'access_token  : {access_token}')
-print(f'refresh_token : {refresh_token}')
+def call_refresh(refresh_token: str) -> tuple[str, str] | None:
+    print('=' * 20 + '  Refresh Token  ' + '=' * 19)
+    response = requests.post(
+        'https://127.0.0.1/refresh', headers={
+            'Authorization': f'Bearer {refresh_token}',
+        }, verify=False,
+    )
 
-print('=' * 20 + '  Refresh Token  ' + '=' * 19)
-json_response = requests.post(
-    'https://127.0.0.1/refresh', headers={
-        'Authorization': f'Bearer {refresh_token}',
-    }, verify=False,
-).json()
-access_token = json_response['access_token']
-refresh_token = json_response['refresh_token']
-print(f'access_token  : {access_token}')
-print(f'refresh_token : {refresh_token}')
+    try:
+        print(response)
+        json_response = response.json()
+        access_token = json_response['access_token']
+        refresh_token = json_response['refresh_token']
+    except:
+        return
 
-print('=' * 20 + '  Refresh Token  ' + '=' * 19)
-json_response = requests.post(
-    'https://127.0.0.1/refresh', headers={
-        'Authorization': f'Bearer {refresh_token}',
-    }, verify=False,
-).json()
-access_token = json_response['access_token']
-refresh_token = json_response['refresh_token']
-print(f'access_token  : {access_token}')
-print(f'refresh_token : {refresh_token}')
+    print(f'access_token  : {access_token}')
+    print(f'refresh_token : {refresh_token}')
+
+    return access_token, refresh_token
+
+
+def call_logout(access_token: str) -> requests.Response:
+    print('=' * 20 + '  Logout  ' + '=' * 26)
+    response = requests.post(
+        'https://127.0.0.1/logout', headers={
+            'Authorization': f'Bearer {access_token}',
+        }, verify=False,
+    )
+
+    return response
+
+
+username = 'johndoe'
+password = 'secret'
+
+access_token, refresh_token = call_authentication(username, password)
+access_token, refresh_token = call_refresh(refresh_token)
+print(call_logout(access_token))
+access_token, refresh_token = call_authentication(username, password)
+access_token, refresh_token = call_authentication(username, password)
+access_token, refresh_token = call_refresh(refresh_token)
+access_token, refresh_token = call_refresh(refresh_token)
+print(call_logout(access_token))
+call_refresh(refresh_token)
+print(call_logout(access_token))
